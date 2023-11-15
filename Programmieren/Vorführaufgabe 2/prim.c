a844f0479ae8e752f94f69cd22c87545e5c8dd0b
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2023-11-15 12:50:52 +0100

    Update prim.c

--- Programmieren/Vorführaufgabe 2/prim.c
+++ Programmieren/Vorführaufgabe 2/prim.c
@@ -1,5 +1,24 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{printf("test");}
+int main() {
+  int wert;
+  int divisor =2;
+
+  printf("bitte geben sie eine Natürliche Zahl ein \n");
+  scanf("%i", &wert);
+  // überprüfen ob eingabe passt
+  if (wert <= 1) {
+    printf("Fehler: eingegebene Zahl <= 1");
+    return -1;
+  }
+  while (wert/divisor!=1) {
+    if (wert % divisor==0)
+    {printf("%i",divisor);
+        wert=wert/divisor;}
+    if (wert%divisor !=0)
+    {divisor++;}
+  }
+  printf("%i", wert);
+}
