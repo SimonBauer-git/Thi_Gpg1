67d5c92ae99ca8164e1afc21969525cc3222e401
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2023-11-22 12:58:18 +0100

    Update fct.c

--- Programmieren/Vorführaufgabe 3/fct.c
+++ Programmieren/Vorführaufgabe 3/fct.c
@@ -2,36 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fct(int x, int n) {
-  switch (n) {
-  case 1:
-    return 2*x-4;
-    break;
-  case 2:
-    return (1/7)+(1/x);
-    break;
-  case 3:
-  printf("3");
-    break;
-  case 4:
-  printf("4");
-    break;
-  case 5:
-  printf("5");
-    break;
-  }
+float fct(double x, int n)
+{
+    switch (n)
+    {
+    case 1:
+        return 2 * x - 4;
+        break;
+    case 2:
+        return (1. / 7) + (1. / x);
+        break;
+    case 3:
+        return (exp(x) - (2 * x) - 3);
+        break;
+    case 4:
+        return (pow((x - 2.0), 3.0) + 2.0);
+        break;
+    case 5:
+        return sin(2 * x);
+        break;
+    }
 }
-int nullstelle(int n, int a, int b, int g, int *ok, int *x0, int *anzit) {
-  *ok = 2;
-  *x0 = 1;
-  *anzit = 1;
+int nullstelle(int n, int a, int b, int g, int *ok, int *x0, int *anzit)
+{
+    *ok = 2;
+    *x0 = 1;
+    *anzit = 1;
 }
 
-int main() {
-  int n, ok, x0, anzit;
-  int fx;
-  fx =fct(1,2);
-  printf("%i", fx);
-  nullstelle(1, 2, 3, 4, &ok, &x0, &anzit);
-  return 0;
+int main()
+{
+    int n, ok, x0, anzit;
+    double fx;
+    double x;
+    prinf("welche Funktion wollen sie berechnen");
+    scanf("%i", &n);
+    prinf("Geben sie einen Wert für x an");
+    scanf("%d", &x);
+    fx = fct(n, x);
+    printf("%d", fx);
+    nullstelle(1, 2, 3, 4, &ok, &x0, &anzit);
+    return 0;
 }
