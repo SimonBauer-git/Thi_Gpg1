fd88a91d8794788d5dd5237302494f76edfe013b
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2023-12-01 18:14:52 +0100

    Update fct.c

--- Programmieren/Vorführaufgabe 3/fct.c
+++ Programmieren/Vorführaufgabe 3/fct.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float fct(float x, int n)
+double fct(double x, int n)
 {
     switch (n)
     {
@@ -25,52 +25,52 @@ float fct(float x, int n)
 }
 void nullstelle(int n, double a, double b, double g, double *x0, int *anzit)
 {
-    double mittelwert = fabs(a + b) / 2;
-    double fx1;
-    double fx2;
-    fx1 = fct(a, n);
-    fx2 = fct(b, n);
-    while (g < fabs(b-a))
-    {   fx1 = fct(a, n);
-        fx2 = fct(b, n);
-        mittelwert=fct((fabs(a+b))/2,n);
-        if ((fx1 > 0 && mittelwert < 0)||(fx1 < 0 && mittelwert > 0))       //schauen ob in im interval a mittelwert ein vorzeichenwechsel ist
+    double mittelwert;
+    a=0;
+    b=16;
+    g=2;
+
+    while (fabs(b - a) > g)
+    {
+        mittelwert = (a + b) / 2;
+
+        if (fct(mittelwert, n) == 0)
         {
-            a=mittelwert;
+            break;
         }
 
-        if ((fx2 > 0 && mittelwert < 0)||(fx2 < 0 && mittelwert > 0))       //schauen ob in im interval b mittelwert ein vorzeichenwechsel ist
+        if ((fct(a,n) > 0 && fct(mittelwert,n) < 0)||(fct(a,n) < 0 && fct(mittelwert,n) > 0)) {
+            b = mittelwert;}
+
+        else
         {
-            b=mittelwert;
+            a = mittelwert;
         }
-        
-    *anzit++; }
-    *x0 = fabs(a + b) / 2;
-    printf("die Nullstelle liegt bei %d",*x0);
+
+       
+    }
+
+    printf("die Nullstelle liegt bei %d", mittelwert);
     printf(", Anzahl der Iterationsschritte: %i", *anzit);
 }
 
 int main()
-{ // 1. Aufgabe
-    int n, anzit;
-    double fx, x0;
+{   int n, anzit;
+    double a, b, g, x0;
+
     printf("welche Funktion wollen sie berechnen\n");
     scanf("%i", &n);
 
-    // 2. Aufgabe
-    double a, b;
-    double g;
-
     printf("geben sie die intervallgrenze a ein\n");
-    scanf("%i", &a);
+    scanf("%d", &a);
 
     printf("geben sie die intervallgrenze b ein\n");
-    scanf("%i", &b);
+    scanf("%d", &b);
 
     printf("geben sie die genauigkeit g ein\n");
-    scanf("%i", &g);
+    scanf("%d", &g);
 
-   nullstelle(n, a, b, g, &x0, &anzit);
-    
+    nullstelle(n, a, b, g, &x0, &anzit);
+
     return 0;
 }
