73c8f1e7c591564cc5efc298cb60c58d264aff70
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2023-12-06 13:57:39 +0000

    Update vigenere.c

--- Programmieren/Vorführaufgabe 4/vigenere.c
+++ Programmieren/Vorführaufgabe 4/vigenere.c
@@ -1,25 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+char alphabet[26] = {'a', 'b' ,'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
+
+void verschlüsseln ()
+{//eingabe in zahlen umwandeln, um diese zahlen das alphabet verschieben
+//dann um diese zahl verschieben, buchstabe pro buchstabe
+//dann die 
+}
+
+void entschlüsseln()
+{}
+
 int main()
 {
-    char schlüssel[][16] = {
-        {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'},
-    };
     int modus;
+    char schlüsselwort [10];
 
     printf("was wollen sie tun(1 = verschlüsseln, 2 = entschlüsseln)\n");
     scanf("%i", &modus);
-
+    printf("geben sie das schlüsselwort an\n");
+    scanf("%s",&schlüsselwort);
+    printf("schlüsselwort: %s\n", schlüsselwort);
+    
     if (modus == 1)
     {
         printf("verschlüsseln");
+        verschlüsseln();
     }
     if (modus == 2)
     {
         printf("entschlüsseln");
+        verschlüsseln();
     }
-    else
+    if (modus !=2 && modus !=1)
     {
         printf("ungültige Eingabe");
         return -1;
