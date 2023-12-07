28505c250276c213ac539c7edde883a3e14a236e
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2023-12-07 11:31:53 +0000

    Update vigenere.c

--- Programmieren/Vorführaufgabe 4/vigenere.c
+++ Programmieren/Vorführaufgabe 4/vigenere.c
@@ -1,28 +1,51 @@
+
+            // eingabe in zahlen umwandeln, um diese zahlen das alphabet verschieben
+            // dann um diese zahl verschieben, buchstabe pro buchstabe
+            // dann die eingegebenen buchstaben den auszugebenden buchstaben zuweisen, ebenfalls für jeden buchstaben
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-char alphabet[26] = {'a', 'b' ,'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
+char alphabet[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
 
-void verschlüsseln ()
-{//eingabe in zahlen umwandeln, um diese zahlen das alphabet verschieben
-//dann um diese zahl verschieben, buchstabe pro buchstabe
-//dann die 
+void wort_in_zahlen(char schlüsselwort[], int *eingabe_z[]) //wandle das codewort das eingegeben wurde in zahlen um, damit man das alphabet shiften kann.
+{int z=0;
+    while (z < strlen(schlüsselwort))
+    {
+        int i;
+        i=0;
+        while (i <= 26 && alphabet[i] != schlüsselwort[z])
+        {
+
+            i++;
+        }
+      
+    z++;
+    printf("%i,",i);}
 }
 
 void entschlüsseln()
-{}
+{
+}
+
+void verschlüsseln()
+{
+}
 
 int main()
 {
     int modus;
-    char schlüsselwort [10];
+    char schlüsselwort[10];
+    int eingabe_z[10];
 
     printf("was wollen sie tun(1 = verschlüsseln, 2 = entschlüsseln)\n");
     scanf("%i", &modus);
     printf("geben sie das schlüsselwort an\n");
-    scanf("%s",&schlüsselwort);
+    scanf("%s", &schlüsselwort);
     printf("schlüsselwort: %s\n", schlüsselwort);
-    
+    wort_in_zahlen(schlüsselwort, eingabe_z);
+
     if (modus == 1)
     {
         printf("verschlüsseln");
@@ -31,9 +54,9 @@ int main()
     if (modus == 2)
     {
         printf("entschlüsseln");
-        verschlüsseln();
+        entschlüsseln();
     }
-    if (modus !=2 && modus !=1)
+    if (modus != 2 && modus != 1)
     {
         printf("ungültige Eingabe");
         return -1;
