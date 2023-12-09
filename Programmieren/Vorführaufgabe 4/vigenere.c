bc3f05773b9c23289fffdea7f357ea29e9ce317a
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2023-12-09 20:57:31 +0100

    Update vigenere.c

--- Programmieren/Vorführaufgabe 4/vigenere.c
+++ Programmieren/Vorführaufgabe 4/vigenere.c
@@ -1,12 +1,12 @@
 // eingabe in zahlen umwandeln, um diese zahlen das alphabet verschieben erledigt
 // dann um diese zahl verschieben, buchstabe pro buchstabe
-// dann die eingegebenen buchstaben den auszugebenden buchstaben zuweisen, ebenfalls für jeden buchstaben
+//dann das ganze ausgeben
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-int modus;
+    int modus;
 
     char schlüsselwort[10];
     int eingabe_s[10];
@@ -15,7 +15,7 @@ int modus;
     int eingabe_t[99];
 
 char alphabet[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
-void schlüsselwort_in_zahlen(char schlüsselwort[], int eingabe_s[]) // wandle das codewort das eingegeben wurde in zahlen um, damit man das alphabet shiften kann.
+void schlüsselwort_in_zahlen()
 {
     int z = 0;
     while (z < strlen(schlüsselwort))
@@ -28,13 +28,13 @@ void schlüsselwort_in_zahlen(char schlüsselwort[], int eingabe_s[]) // wandle 
             i++;
         }
 
-        z++;
+        
+        
         eingabe_s[z] = i;
-    }
-
+    z++;}
 }
 
-void text_in_zahlen(char text[], int eingabe_t[]) // wandle den text der eingegeben wurde in zahlen um, damit man das alphabet shiften kann.
+void text_in_zahlen() // wandle den text der eingegeben wurde in zahlen um, damit man das alphabet shiften kann.
 {
     int z = 0;
     while (z < strlen(text))
@@ -47,35 +47,26 @@ void text_in_zahlen(char text[], int eingabe_t[]) // wandle den text der eingege
             i++;
         }
 
-        z++;
+        
         eingabe_t[z] = i;
-    }
+    z++;}
 
-printf("%i",eingabe_t[0]);}
-
-/*void tabelle_erstellen(int eingabe_t[]) // erstelle die schlüsseltabelle in einem mehrdimensionalen array
-{
-    int i = 0;
-    while (i < 27)
-    { // verschiebe die erste zeile um den ersten buchstaben
-        schlüsseltabelle[i][0] = alphabet[eingabe_t[0] + i];
-        printf("%c", schlüsseltabelle[0][0]);
-        i++;
-    }
-    
-}*/
+}
 
 
 void entschlüsseln()
 {
 }
 
-void verschlüsseln(int eingabe_s[], int eingabe_t[])
-{printf("%c" ,alphabet[eingabe_s[0]+eingabe_t[0]]);
+void verschlüsseln()
+{
+    printf("%c" ,alphabet[eingabe_s[0]+eingabe_t[0]]);
 }
 
 int main()
 {
+
+
     printf("was wollen sie tun(1 = verschlüsseln, 2 = entschlüsseln)\n");
     scanf("%i", &modus);
     printf("geben sie das schlüsselwort an\n");
@@ -84,13 +75,13 @@ int main()
     printf("geben sie den gewünschten text ein:\n");
     scanf("%s",&text);
 
-    schlüsselwort_in_zahlen(schlüsselwort, eingabe_s);
-    text_in_zahlen(text, eingabe_t);
+    schlüsselwort_in_zahlen();
+    text_in_zahlen();
 
     if (modus == 1)
     {
        
-        verschlüsseln(eingabe_s, eingabe_t);
+        verschlüsseln();
     }
     if (modus == 2)
     {
